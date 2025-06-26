b15870d639da9a85c4918b2ae1dca9aefeb8e4c7
sstrange259 <80662134+sstrange259@users.noreply.github.com>
2025-06-25 21:19:52 -0400

    Initial Commit

---
+++ EchoEdit/EchoEditApp.swift
@@ -0,0 +1,32 @@
+//
+//  EchoEditApp.swift
+//  EchoEdit
+//
+//  Created by Steven Strange on 6/25/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct EchoEditApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
